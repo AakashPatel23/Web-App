b8b2ac4cd10b3f6841a28f0a5815da9be9fb18c6
AakashPatel23 <pate2011@purdue.edu>
2025-03-29 15:24:54 -0400

    update models

--- backend/models/Expense.js
+++ backend/models/Expense.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const ExpenseSchema = new mongoose.Schema(
   {
-    _id: mongoose.Schema.Types.ObjectId, 
+    _id: mongoose.Schema.Types.ObjectId,
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     name: { type: String, required: true },
     amount: { type: Number, required: true },
@@ -10,8 +10,8 @@ const ExpenseSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "Category",
       required: true,
-    }, 
-    date: { type: Date, default: Date.now }, 
+    },
+    date: { type: Date, default: Date.now },
     notes: { type: String, default: null },
   },
   { collection: "expenses" }
