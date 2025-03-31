acbe308228484da51e0a0c6d99f160ecbb57fa9a
AakashPatel23 <pate2011@purdue.edu>
2025-03-31 10:53:12 -0400

    fixed file structure + chakra first step

---
+++ backend/src/routes/expense.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+import {
+  createExpense,
+  getAllExpenses,
+  deleteExpense,
+  getAllExpensesByCategory,
+  getExpenseById,
+  updateExpense,
+} from "../controllers/expense.js";
+const router = express.Router();
+
+export default router;
+
+// Expense Create
+router.post("/", createExpense);
+
+// Expense Delete
+router.delete("/:expenseId", deleteExpense);
+
+// Get all expenses for a user while sanitizing the input
+router.get("/:userId", getAllExpenses);
+
+// Get expenses by category for a user while sanitizing the input
+router.get("/:userId/category/:categoryId", getAllExpensesByCategory);
+
+// Get expense by ID while sanitizing the input
+router.get("/id/:expenseId", getExpenseById);
+
+// Update expense while sanitizing the input
+router.patch("/:expenseId", updateExpense);
