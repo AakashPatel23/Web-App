abdb87cf0a234a219cc21eacef344c3bd56c64e2
AakashPatel23 <pate2011@purdue.edu>
2025-05-01 17:53:12 -0400

    pre postman test

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,7 @@
 import express from "express";
 import cors from "cors";
-//import records from "./routes/record.js";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import userRoute from "./routes/user.js";
 import categoryRoute from "./routes/category.js";
 import expenseRoute from "./routes/expense.js";
 
@@ -14,7 +12,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/users", userRoute); // Use the user route for handling user-related requests
 app.use("/api/categories", categoryRoute); // Use the category route for handling category-related requests
 app.use("/api/expenses", expenseRoute); // Use the expense route for handling expense-related requests
 
